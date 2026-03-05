2058bf393d7499baf674f7386cd6d18189f26d98
Johan Backman <johan.backman@affirm.com>
2026-03-04 21:30:20 -0800

    Fix linter errors

--- lib/ysc_web/live/admin/admin_events_live.ex
+++ lib/ysc_web/live/admin/admin_events_live.ex
@@ -383,15 +383,6 @@ defmodule YscWeb.AdminEventsLive do
     do_clear_search(socket, input_id)
   end
 
-  defp do_clear_search(socket, input_id) do
-    new_params = Map.delete(socket.assigns[:params], "search")
-
-    {:noreply,
-     socket
-     |> assign(:focus_search_input, input_id)
-     |> push_patch(to: ~p"/admin/events?#{new_params}")}
-  end
-
   def handle_event("update-filter", params, socket) do
     date_from = Map.get(params, "date_from", "")
     date_to = Map.get(params, "date_to", "")
