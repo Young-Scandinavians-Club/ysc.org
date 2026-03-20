1aa4c81e6d8aa143ba8a6fa2a8e581b53cea476b
Johan Backman <johan.backman@affirm.com>
2026-03-20 09:23:44 -0700

    Update mig

--- priv/repo/migrations/20260320120000_add_post_migration_onboarding_to_users.exs
+++ priv/repo/migrations/20260320120000_add_post_migration_onboarding_to_users.exs
@@ -5,13 +5,5 @@ defmodule Ysc.Repo.Migrations.AddPostMigrationOnboardingToUsers do
     alter table(:users) do
       add :post_migration_onboarding_completed_at, :utc_datetime, null: true
     end
-
-    # Backfill all existing users so they are not shown the onboarding wizard.
-    # Only new WP-migrated users (inserted after this migration with a nil value)
-    # will be required to complete onboarding.
-    execute(
-      "UPDATE users SET post_migration_onboarding_completed_at = inserted_at",
-      "UPDATE users SET post_migration_onboarding_completed_at = NULL"
-    )
   end
 end
