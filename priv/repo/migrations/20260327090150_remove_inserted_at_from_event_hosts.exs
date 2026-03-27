e5a987cea09def8671db768e59461428b111e9cd
Johan Backman <johan.backman@affirm.com>
2026-03-27 09:48:08 -0700

    Fix migration

--- priv/repo/migrations/20260327090150_remove_inserted_at_from_event_hosts.exs
+++
@@ -1,9 +0,0 @@
-defmodule Ysc.Repo.Migrations.RemoveInsertedAtFromEventHosts do
-  use Ecto.Migration
-
-  def change do
-    alter table(:event_hosts) do
-      remove :inserted_at
-    end
-  end
-end
