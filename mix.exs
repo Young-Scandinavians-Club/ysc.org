c5c0ef85e80f7c6a7f59454469c809d0770b86a6
Johan Backman <johan.backman@affirm.com>
2026-04-27 10:57:42 -0700

    Bump version to 1.0.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ysc.MixProject do
   def project do
     [
       app: :ysc,
-      version: "1.0.8",
+      version: "1.0.9",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
