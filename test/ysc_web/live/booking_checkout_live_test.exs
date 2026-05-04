810ad8bcdc98c82bd52e627827b020d269e4561e
Johan Backman <backman93@gmail.com>
2026-05-03 21:28:19 -0700

    Verify on exit

--- test/ysc_web/live/booking_checkout_live_test.exs
+++ test/ysc_web/live/booking_checkout_live_test.exs
@@ -15,6 +15,7 @@ defmodule YscWeb.BookingCheckoutLiveTest do
   alias Ysc.StripeMock
 
   setup :set_mox_global
+  setup :verify_on_exit!
 
   setup %{conn: conn} do
     Ledgers.ensure_basic_accounts()
