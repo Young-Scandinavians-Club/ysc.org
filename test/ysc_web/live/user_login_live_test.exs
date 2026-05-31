9ae8aa6239b87f64e5ca32fb803e3a8e022d6f1d
Johan Backman <backman93@gmail.com>
2026-05-31 09:40:02 -0700

    Restore Passkey reference on login page

--- test/ysc_web/live/user_login_live_test.exs
+++ test/ysc_web/live/user_login_live_test.exs
@@ -37,7 +37,7 @@ defmodule YscWeb.UserLoginLiveTest do
 
       html = render(lv)
 
-      assert html =~ "Sign in with fingerprint or face" ||
+      assert html =~ "Sign in with Passkey" ||
                html =~ "Sign in with Face ID"
     end
 
