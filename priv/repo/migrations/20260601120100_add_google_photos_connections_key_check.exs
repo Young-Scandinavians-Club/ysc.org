80eaa6953c2493f353e394d90048bbef7a184a42
Johan Backman <johan.backman@affirm.com>
2026-06-01 11:23:06 -0700

    Fix

--- priv/repo/migrations/20260601120100_add_google_photos_connections_key_check.exs
+++
@@ -1,7 +0,0 @@
-defmodule Ysc.Repo.Migrations.AddGooglePhotosConnectionsKeyCheck do
-  use Ecto.Migration
-
-  def change do
-    create constraint(:google_photos_connections, :only_default_key, check: "key = 'default'")
-  end
-end
